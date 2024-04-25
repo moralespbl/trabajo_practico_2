c85530f7423782694b9dd2f172cbcf9941d1616b
Pablo <Pablo@DESKTOP-L7A7O98>
2024-04-25 12:13:34 -0300

    Ejercicio 3 terminado

---
+++ tp2_MoralesValerianoPablo/src/ar/edu/unju/fi/ejercio3/constantes/Provincia.java
@@ -0,0 +1,31 @@
+package ar.edu.unju.fi.ejercio3.constantes;
+
+public enum Provincia {	
+	    JUJUY(797955L, 53244L), 
+	    SALTA(1440672L, 155340L), 
+	    TUCUMAN(1703186L, 22592L), 
+	    CATAMARCA(429556L, 101486L), 
+	    LA_RIOJA(384607L, 91493L), 
+	    SANTIAGO_DEL_ESTERO(1054028L, 136934L);
+
+	    private long POBLACION;
+	    private long SUPERFICIE;
+
+	    private Provincia(long POBLACION, long SUPERFICIE) {
+	        this.POBLACION = POBLACION;
+	        this.SUPERFICIE = SUPERFICIE;
+	    }
+
+	    public long getPoblacion() {
+	        return POBLACION;
+	    }
+
+	    public long getSuperficie() {
+	        return SUPERFICIE;
+	    }
+	    
+	    public long calcularDensidad(long POBLACION, long SUPERFICIE) {
+	    	return POBLACION/SUPERFICIE;
+	    }
+
+}
---
+++ tp2_MoralesValerianoPablo/src/ar/edu/unju/fi/ejercio3/main/Main.java
@@ -0,0 +1,18 @@
+package ar.edu.unju.fi.ejercio3.main;
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.edu.unju.fi.ejercio3.constantes.Provincia;
+
+public class Main {
+	public static List<Provincia> provincias = new ArrayList<>(List.of(Provincia.values()));
+	public static void main(String[] args) {
+		System.out.println("---Lista de Provincias ----");
+		for (Provincia provincia: provincias) {
+			System.out.println("\nProvincia: "+provincia);
+			System.out.println("Poblacion: "+provincia.getPoblacion()+" hab.");
+			System.out.println("Superficie: "+provincia.getSuperficie()+" km2");
+			System.out.println("Densidad: "+provincia.calcularDensidad(provincia.getPoblacion(), provincia.getSuperficie())+" hab. por km2");			
+		}
+	}
+}
